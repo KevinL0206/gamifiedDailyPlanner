30c789d256f75b1c8244bf71d3c785f6f37dcfcd
KevinL <90875029+KevinL0206@users.noreply.github.com>
2023-12-28 02:17:28 +0000

    display completed status

--- dailyplanner/planner/views.py
+++ dailyplanner/planner/views.py
@@ -11,9 +11,29 @@ def displaySchedule(request):
     currentUser = request.user
     userInstance = User.objects.get(username = currentUser)
     scheduleInstance = schedule.objects.get(user = userInstance)
+    userProfileInstance = userProfile.objects.get(user = userInstance)
 
-    
+    experience = userProfileInstance.experience
+    level = userProfileInstance.level
+    maxXP = userProfileInstance.maxXP
     tasks = scheduleInstance.taskID.all()
-    context = {'tasks': tasks}
+    task_ids = [task.taskID for task in tasks]
+    taskInfos = [task.taskInfo for task in tasks]
+    flags = []
+
+    for id in task_ids:
+        completedInstance = completed.objects.get(userID = userProfileInstance, taskID = id)
+        flags.append(completedInstance.completedFlag)
+
+    taskzip = zip(taskInfos,flags)
+
+    context = {
+        'user' : currentUser,
+        'tasks' : taskzip,
+        'experience' : experience,
+        'max' : maxXP,
+        'level' : level,
+        
+        }
 
     return render(request,'schedule.html',context)
